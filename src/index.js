f333fb667f3010b4487ed08f186e36e7973290c0
Gui <guilhermehenry60@gmail.com>
2020-02-15 13:04:28 -0300

    feat: add Header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+
+import App from './App'
 import './styles/global.css'
 
 
-ReactDom.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('root'));
